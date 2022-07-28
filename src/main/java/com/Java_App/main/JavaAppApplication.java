e9347500be884588b530d425fa7f91c59cac4d06
MJI-Abir <mjiabir12007@gmail.com>
2022-07-28 21:02:31 +0600

    first commit

---
+++ src/main/java/com/Java_App/main/JavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.Java_App.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppApplication.class, args);
+	}
+
+}
